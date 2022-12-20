f4f810c4716fcc669406d3fc4af4c33109f17c75
Jack Williams <jackw@orphans.co.uk>
2022-12-20 13:14:22 +0000

    404.php added

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -1,11 +1,12 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>><!-- gives every article a unique ID -->
 	
 	<header class="entry-header">
-		<h1><?php esc_html_e('Error 404', 'wphierarchy'); ?></h1>
+		<h1><?php esc_html_e('Error 404 - Page Not Found!', 'wphierarchy'); ?></h1>
 	</header>
 
 	<div class="entry-content">
-		<p><?php esc_html_e('No content found, sorry', 'wphierarchy'); ?></p>
+		<p><?php esc_html_e('Sorry! We do not seem to have the content you are looking for.', 'wphierarchy'); ?></p>
+		<p><?php echo get_search_form(); ?></p>
 	</div>
 
 </article> 
